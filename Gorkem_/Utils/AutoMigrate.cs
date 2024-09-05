2ac2d39594d03493368c2a9d89f5405427edcef2
necipbaran <alexza42@hotmail.com>
2024-09-05 14:46:57 +0300

    ..

---
+++ Gorkem_/Utils/AutoMigrate.cs
@@ -0,0 +1,25 @@
+﻿using Gorkem_.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Gorkem_.Utils
+{
+    public static class AutoMigrate
+    {
+      public  static void ApplyMigration(WebApplication app)
+        {
+            using (var scope = app.Services.CreateScope())
+            {
+                var _Db = scope.ServiceProvider.GetRequiredService<GorkemDbContext>();
+                if (_Db != null)
+                {
+                    if (_Db.Database.GetPendingMigrations().Any())
+                    {
+                        _Db.Database.Migrate();
+                    }
+                }
+            }
+        }
+
+    }
+}
