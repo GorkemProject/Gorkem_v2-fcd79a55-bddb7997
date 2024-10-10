ddd1b5c66ffbeb3fbeec80ff95bdacf2ec6a8c72
NARKOBT <NARKOBT@DESKTOP-FK3J49F>
2024-10-10 15:05:37 +0300

    Köpek tablosu ve edinim şekilleri birleştirildi.

--- Gorkem_/Contracts/Kopek/KopekGetirResponse.cs
+++ Gorkem_/Contracts/Kopek/KopekGetirResponse.cs
@@ -11,13 +11,20 @@ namespace Gorkem_.Contracts.Kopek
         public int BransId { get; set; }
         public string? KuvveNumarasi { get; set; }
         public string? CipNumarasi { get; set; }
-        public int Karar { get; set; }
         public DateTime T_Aktif { get; set; }
         public DateTime? T_Pasif { get; set; }
         public DateTime DogumTarihi { get; set; }
         public string? YapilanIslem { get; set; }
         public string? NihaiKanaat { get; set; }
+        public int KararId { get; set; }
+        public Enum_Cinsiyet Cinsiyet { get; set; }
         public Enum_TeminSekli EdinimSekli { get; set; }
+        public int? AnneKopekId { get; set; }
+        public int? BabaKopekId { get; set; }
+        public string? EdinilenKisi { get; set; }
+        public string? EdinilenKisiAdres { get; set; }
+        public string? EdinilenKisiTelefon { get; set; }
+        public DateTime? EdinilmeTarihi { get; set; }
 
 
     }
