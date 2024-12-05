8780298099459483fcbc38dd7fe8f9c502218ad3
Oguzhaan7 <oguzhandag88@gmail.com>
2024-12-05 10:54:16 +0300

    merged

--- Gorkem_/Contracts/KopekKurs/KopekKursGetirFilterResponse.cs
+++ Gorkem_/Contracts/KopekKurs/KopekKursGetirFilterResponse.cs
@@ -4,7 +4,7 @@ namespace Gorkem_.Contracts.KopekKurs
 {
     public class KopekKursGetirFilterResponse
     {
-
+        public int Id { get; set; }
         public DateTime? T_KursBaslangic { get; set; }
         public DateTime? T_KursBitis { get; set; }
         public int? KursYeriId { get; set; }
